d6f182ebb94ca68401cac988d880891ee7955e86
Jake6654 <ugw2ad@virginia.edu>
2026-03-03 16:37:47 -0500

    Add backend project

---
+++ backend/src/test/java/sketch_my_day/demo/SketchMyDayApplicationTests.java
@@ -0,0 +1,13 @@
+package sketch_my_day.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SketchMyDayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
